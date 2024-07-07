74108219a4f87be645438592f2af2af9c21054de
Nahuel Aguirre MacBook Pro <nahuelaguirre@Nahuels-MacBook-Pro.local>
2024-07-07 17:55:47 -0600

    Se ajustó en JS, se cambió usando nuevas funciones con condicionales, pero la consola me da un error en el addEvetListener que no se como solucionar

--- JS/script.js
+++ JS/script.js
@@ -1,35 +1,75 @@
-const nombre = document.getElementById("name");
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
 const email = document.getElementById("email");
 const pass = document.getElementById("password");
+const passConfirm = document.getElementById("password2");
 
-const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
 
-form.addEventListener("submit", e => {
-    e.preventDefault()
-    let warnings = ""
-    let entrar = false
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
-    parrafo.innerHTML = ""
-    if (nombre.value.length < 6) {
-        warnings += `El nombre no es valido <br>`
-        entrar = true
-    }
-    if (!regexEmail.test(email.value)) {
-        warnings += `El email no es valido <br>`
-        entrar = true
-    }
-    if (pass.value.length < 6) {
-        warnings += `La contraseña no es valida <br>`
-        entrar = true
-    }
+form.addEventListener('submit', e => {
+    e.preventDefault();
 
-    if (entrar) {
-        parrafo.innerHTML = warnings
-    } else {
-        parrafo.innerHTML = "Enviado"
-    }
+    validateInputs();
 });
 
-console.log('Formulario enviado');
-console.log(values);
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+
+};
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error')
+};
+
+const isValidEmail = email => {
+    const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    return regexEmail.test(string(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const nombreValue = nombre.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if (nombreValue === '') {
+        setError(nombre, 'Escriba su Nombre');
+    } else {
+        setSuccess(nombre);
+    };
+
+    if (emailValue === '') {
+        setError(email, 'Escriba su Correo');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Escriba un Correo válido');
+    } else {
+        setSuccess(email);
+    };
+
+    if (passwordValue === '') {
+        setError(password, 'Escriba su password');
+    } else if (passwordValue.length < 8) {
+        setError(password, 'Su password necesita mas de 8 caracteres.')
+    } else {
+        setSuccess(password);
+    };
+
+    if (password2Value === '') {
+        setError(password2, 'Confirme su password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, 'Su confirmación no es correta.')
+    } else {
+        setSuccess(password2);
+    };
+};
+
+
